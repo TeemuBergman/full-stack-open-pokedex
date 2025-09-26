5ca4f3719ad539e523f00cd193de1f103006c5f3
TeemuBergman <teemu.bergman@helsinki.fi>
2025-09-26 23:38:49 +0300

    11.12

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get('/version', (req, res) => {
   })
 })
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
