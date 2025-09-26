1aa47c147e57635fac34b2b7b614ea344a4d96fe
TeemuBergman <teemu.bergman@helsinki.fi>
2025-09-26 22:34:19 +0300

    11.10

--- app.js
+++ app.js
@@ -6,6 +6,15 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+const serverStart = new Date().toISOString()
+
+app.get('/version', (req, res) => {
+  res.json({
+    version: '1.0.0',
+    startedAt: serverStart
+  })
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
